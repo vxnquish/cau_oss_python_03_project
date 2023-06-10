c084009002ed3923551e46b639042b3b726c565c
qabin <qabinkicks@gmail.com>
2023-06-10 23:18:49 +0900

    Version #2

--- menu_selector.py
+++ menu_selector.py
@@ -1,4 +1,12 @@
+import file_manager
+import parking_spot_manager
+
 def start_process(path):
+    temp_list = file_manager.read_file(path) # file_manager.py 에 있는 read_file 함수에서 return 값인 list_str 을 temp_list 에 저장한다.
+    main_list = parking_spot_manager.str_list_to_class_list(temp_list)  # parking_spot_manager.py 에 있는
+                                                                        # str_list_to_class_list 함수에서
+                                                                        # return 값인 class_list 를 main_list 에 저장한다.
+
     while True:
         print("---menu---")
         print("[1] print")
@@ -7,8 +15,10 @@ def start_process(path):
         print("[4] exit")
         select = int(input('type:'))
         if select == 1:
-            print("not implemented yet")
-            # fill this block
+            parking_spot_manager.print_spots(main_list) # 1 을 입력했을 경우,
+                                                        # parking_spot_manager.py 에 있는
+                                                        # print_spots 함수를 호출한다.
+
         elif select == 2:
             print("---filter by---")
             print("[1] name")
@@ -42,6 +52,7 @@ def start_process(path):
                 # fill this block
             else:
                 print("invalid input")
+
         elif select == 3:
             keywords = ['name', 'city', 'district', 'ptype', 'latitude', 'longitude']
             print("---sort by---")
@@ -51,8 +62,11 @@ def start_process(path):
                 print("not implemented yet")
                 # fill this block
             else: print("invalid input")
+
         elif select == 4:
-            print("not implemented yet")
-            # fill this block
+            print("Exit")   # 4 를 입력했을 경우,
+            break           # Exit 을 출력하고,
+                            # 반복문을 빠져나가 함수를 종료한다.
+
         else:
             print("invalid input")
--- parking_spot_manager.py
+++ parking_spot_manager.py
@@ -1,12 +1,28 @@
 class parking_spot:
-    # you have to implement 'constructor(생성자)' and 'get' method
-    def __str__(self):
+    def __init__(self, name, city, district, ptype, longitude, latitude): # 생성자
+        self.__item = {'name': name, 'city': city, 'district': district, 'ptype': ptype, 'longitude': longitude, 'latitude': latitude}
+
+    def get(self, keyword = 'name'): # get 함수
+        return self.__item[keyword]
+
+    def __str__(self): # 문자열로 변환할 때 수행할 내용
         item = self.__item
         s  = f"[{item['name']}({item['ptype']})] "
         s += f"{item['city']} {item['district']}"
         s += f"(lat:{item['latitude']}, long:{item['longitude']})"
         return s
+    
+def str_list_to_class_list(str_list):
+    class_list = list()
+    for i in str_list:
+        spl = str(i).split(',') # str_list 의 원소인 문자열 i 를 , (쉼표) 를 기준으로 split 하고, 이를 spl 에 저장한다.
+        class_list.append(parking_spot(spl[1], spl[2], spl[3], spl[4], float(spl[5]), float(spl[6]))) # spl[0] 은 [순번] 이므로 제외함.
+    return class_list
 
+def print_spots(spots): # parking_spot 클래스의 __str__ 을 이용하여 출력한다.
+    print(f"---print elements({len(spots)})---")
+    for spot in spots:
+        print(str(spot))
 
 # 각 단계별로 테스트 (테스트할때 주석해제 후 사용)
 if __name__ == '__main__':
